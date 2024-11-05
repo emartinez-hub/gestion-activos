7e9ddd61693bc513dbc71b46679fa19b2dfd672b
Eyleen <eyleenmartinez829@gmail.com>
2024-11-05 19:48:29 -0400

    Formateadores agregados

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,13 +1,13 @@
-import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css';
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    theme:{
-        defaultTheme:'light'
-    }
-  })
-  app.vueApp.use(vuetify)
-})
+    theme: {
+      defaultTheme: 'light',
+    },
+  });
+  app.vueApp.use(vuetify);
+});

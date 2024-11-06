0871a145d5a9e367068aa33e155e32218f6da842
ImDev11 <euris110811@gmail.com>
2024-11-05 20:19:25 -0400

    chore: routing enabled and default layout created

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-main
+      class="d-flex align-center justify-center bg-main"
+      style="min-height: 300px"
+    >
+      <slot />
+    </v-main>
+  </v-layout>
+</template>

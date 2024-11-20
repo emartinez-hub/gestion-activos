18a576e1c268a9eab4e0884635e934d910ce4b13
ImDev11 <euris110811@gmail.com>
2024-11-20 00:37:05 -0400

    build: ssr disabled

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,23 +2,23 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  ssr: false,
   devtools: { enabled: true },
-    build: {
-      transpile: ['vuetify'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify({ autoImport: true }))
+      })
     },
-    modules: [
-      (_options, nuxt) => {
-        nuxt.hooks.hook('vite:extendConfig', (config) => {
-          // @ts-expect-error
-          config.plugins.push(vuetify({ autoImport: true }))
-        })
-      },
-    ],
-    vite: {
-      vue: {
-        template: {
-          transformAssetUrls,
-        },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
       },
     },
+  },
 })

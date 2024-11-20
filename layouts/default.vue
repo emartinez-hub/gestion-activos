21056572326c079731cab285ed35dd96657dd2c0
ImDev11 <euris110811@gmail.com>
2024-11-20 00:38:22 -0400

    feat: layout improvements

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,67 +1,107 @@
 <template>
-  <v-layout>
-    <v-app-bar>
-      <v-app-bar-nav-icon
-        variant="text"
-        @click.stop="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+  <v-card>
+    <v-layout>
+      <v-app-bar prominent>
+        <v-app-bar-nav-icon
+          variant="text"
+          @click.stop="drawer = !drawer"
+        ></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Gestión de Activos</v-toolbar-title>
+        <v-toolbar-title>Gestión de Activos</v-toolbar-title>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <template v-if="$vuetify.display.mdAndUp">
-        <v-btn icon="mdi-magnify" variant="text"></v-btn>
-        <v-btn icon="mdi-filter" variant="text"></v-btn>
-      </template>
+        <v-btn @click="toggleTheme" icon="mdi-theme-light-dark" variant="text"></v-btn>
+      </v-app-bar>
 
-      <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
-    </v-app-bar>
+      <v-navigation-drawer
+        v-model="drawer"
+        :temporary="$vuetify.display.mobile"
+      >
+        <v-divider></v-divider>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      :location="$vuetify.display.mobile ? 'bottom' : undefined"
-      temporary
-      class="push"
-    >
-      <v-list :items="items"></v-list>
-    </v-navigation-drawer>
+        <v-list density="compact" nav>
+          <nuxt-link to="/">
+            <v-list-item
+              prepend-icon="mdi-view-dashboard"
+              title="Dashboard"
+              value="dashboard"
+            />
+          </nuxt-link>
 
-    <v-main :class="{ 'shifted-main': drawer }" class="d-flex justify-center mt-15 pt-15 px-3" style="height: 500px">
-      <slot />
-    </v-main>
-  </v-layout>
+          <nuxt-link to="/operations">
+            <v-list-item
+              prepend-icon="mdi-cog-transfer"
+              title="Operaciones"
+              value="operations"
+            />
+          </nuxt-link>
+
+          <nuxt-link to="/shipping">
+            <v-list-item
+              prepend-icon="mdi-package-variant"
+              title="Shipping"
+              value="shipping"
+            />
+          </nuxt-link>
+
+          <nuxt-link to="/customer-service">
+            <v-list-item
+              prepend-icon="mdi-headset"
+              title="Servicio al cliente"
+              value="customer-service"
+            />
+          </nuxt-link>
+
+          <nuxt-link to="/finances">
+            <v-list-item
+              prepend-icon="mdi-currency-usd"
+              title="Finanzas"
+              value="finances"
+            />
+          </nuxt-link>
+
+          <nuxt-link to="/settings">
+            <v-list-item
+              prepend-icon="mdi-cog-outline"
+              title="Configuraciones"
+              value="settings"
+            />
+          </nuxt-link>
+
+          <nuxt-link to="/reports">
+            <v-list-item
+              prepend-icon="mdi-file-chart"
+              title="Reportes"
+              value="reports"
+            />
+          </nuxt-link>
+
+          <nuxt-link to="/my-tickets">
+            <v-list-item
+              prepend-icon="mdi-ticket-account"
+              title="Mis tickets"
+              value="my-tickets"
+            />
+          </nuxt-link>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <slot />
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
+import { useTheme } from 'vuetify';
 
 const drawer = ref(true);
-const group = ref(null);
-const items = ref([
-  { title: 'Foo', value: 'foo' },
-  { title: 'Bar', value: 'bar' },
-  { title: 'Fizz', value: 'fizz' },
-  { title: 'Buzz', value: 'buzz' },
-]);
+const theme = useTheme();
 
-watch(group, () => {
-  drawer.value = false;
-});
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+}
 </script>
-
-<style scoped>
-.push {
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 240px;
-  transition: transform 0.3s ease;
-}
-
-.shifted-main {
-  margin-left: 240px;
-  transition: margin-left 0.3s ease;
-}
-</style>

fb4250435dbfda9bcbaf8b5875dfed1099bc105d
ImDev11 <euris110811@gmail.com>
2024-11-20 00:39:48 -0400

    feat: pages created

---
+++ pages/reports.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+    <v-app-bar v-if="$vuetify.display.mdAndUp" :elevation="1">
+      <v-toolbar-title>Reportes</v-toolbar-title>
+    </v-app-bar>
+
+    <v-breadcrumbs class="pa-0">
+      <v-breadcrumbs-item>
+        <nuxt-link to="/">Home</nuxt-link>
+      </v-breadcrumbs-item>
+      <v-breadcrumbs-divider />
+      <v-breadcrumbs-item disabled>Reportes</v-breadcrumbs-item>
+    </v-breadcrumbs>
+  </v-container>
+</template>
